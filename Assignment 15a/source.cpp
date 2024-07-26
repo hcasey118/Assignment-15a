bab55280cf4630d9059de5e092a8b3dc3d89de82
chris <chris@BIGFELLA>
2024-07-26 17:57:57 -0400

    Add project files.

---
+++ Assignment 15a/source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdexcept>
+
+// Hunter Casey
+// CIS 1202 
+// July 26 2024
+
+// Custom exception for invalid character input
+class invalidCharacterException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Invalid character input!";
+    }
+};
+
+// Custom exception for invalid range
+class invalidRangeException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Invalid character range!";
+    }
+};
